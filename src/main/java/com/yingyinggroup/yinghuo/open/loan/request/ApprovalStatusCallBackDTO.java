0356232f8d92d4c3e99bd6640961ec9b603e7e75
zhapengcheng <zhapengcheng@yingyinggroup.com>
2018-12-19 17:21:26 +0800

    审核结果DTO

---
+++ src/main/java/com/yingyinggroup/yinghuo/open/loan/request/ApprovalStatusCallBackDTO.java
@@ -0,0 +1,52 @@
+package com.yingyinggroup.yinghuo.open.loan.request;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.io.Serializable;
+
+@ApiModel(description = "审核结果DTO")
+@Data
+@AllArgsConstructor
+public class ApprovalStatusCallBackDTO implements Serializable {
+
+    @ApiModelProperty(notes = "订单号", required = true)
+    private String orderNo;
+
+    @ApiModelProperty(notes = "审核状态", required = true)
+    private String approvalStatus;
+
+    @ApiModelProperty(notes = "审核时间(YYYY-MM-dd HH:mm:ss)", required = true)
+    private String approvalTime;
+
+    @ApiModelProperty(notes = "期数", required = true)
+    private int term;
+
+    @ApiModelProperty(notes = "期数类型", required = true)
+    private String termType;
+
+    @ApiModelProperty(notes = "审核利率(万分之一)", required = true)
+    private int rate;
+
+    @ApiModelProperty(notes = "利率类型", required = true)
+    private int rateType;
+
+    @ApiModelProperty(notes = "审核结果说明", required = true)
+    private String remark;
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder("ApprovalStatusCallBackDTO{");
+        sb.append("orderNo='").append(orderNo).append('\'');
+        sb.append("approvalStatus='").append(approvalStatus).append('\'');
+        sb.append("approvalTime='").append(approvalTime).append('\'');
+        sb.append("term='").append(term).append('\'');
+        sb.append("termType='").append(termType).append('\'');
+        sb.append("rate='").append(rate).append('\'');
+        sb.append("rateType='").append(rateType).append('\'');
+        sb.append("remark='").append(remark).append('\'');
+        return sb.toString();
+    }
+}
