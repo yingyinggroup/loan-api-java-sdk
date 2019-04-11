52e075ae9c72a805ac9294521ee1adcd14741b07
杨延彬 <zgsoft_happy@126.com>
2019-04-11 14:11:19 +0800

    add interface

--- src/main/java/com/yingyinggroup/yinghuo/open/loan/api/LoanWebHookApi.java
+++ src/main/java/com/yingyinggroup/yinghuo/open/loan/api/LoanWebHookApi.java
@@ -1,9 +1,7 @@
 package com.yingyinggroup.yinghuo.open.loan.api;
 
 import com.yingyinggroup.yinghuo.open.loan.LoanApiClient;
-import com.yingyinggroup.yinghuo.open.loan.request.ApprovalStatusCallBackDTO;
-import com.yingyinggroup.yinghuo.open.loan.request.BankCardBindingNotificationDTO;
-import com.yingyinggroup.yinghuo.open.loan.request.OrderStatusCallBackDTO;
+import com.yingyinggroup.yinghuo.open.loan.request.*;
 import com.yingyinggroup.yinghuo.open.loan.response.NotificationResponseDTO;
 import feign.Headers;
 import feign.RequestLine;
@@ -40,6 +38,7 @@ public interface LoanWebHookApi extends LoanApiClient.Api {
      */
     @RequestLine("POST /open-api/loan-market/merchant/order_status_notification")
     @Headers("Content-Type: application/json")
+    @Deprecated
     NotificationResponseDTO orderStatusNotification(OrderStatusCallBackDTO bankCardBindingNotificationDTO);
 
 
@@ -51,6 +50,33 @@ public interface LoanWebHookApi extends LoanApiClient.Api {
     @RequestLine("POST /open-api/loan-market/merchant/order_invalid_notification")
     @Headers("Content-Type: application/json")
     NotificationResponseDTO orderInvalidNotification(String orderNo);
+
+    /**
+     * 放款结果回调
+     * @param makeLoanStatusNotificationDTO
+     * @return
+     */
+    @RequestLine("POST /open-api/loan-market/merchant/make_loan_status")
+    @Headers("Content-Type: application/json")
+    NotificationResponseDTO makeLoanStatusNotificationDTO(MakeLoanStatusNotificationDTO makeLoanStatusNotificationDTO);
+
+    /**
+     * 订单状态回调
+     * @param orderStatusNotificationDTO
+     * @return
+     */
+    @RequestLine("POST /open-api/loan-market/merchant/order_status_notification/v2")
+    @Headers("Content-Type: application/json")
+    NotificationResponseDTO orderStatusNotification(OrderStatusNotificationDTO orderStatusNotificationDTO);
+
+    /**
+     * 还款结果回调
+     * @param repayStatusNotificationDTO
+     * @return
+     */
+    @RequestLine("POST /open-api/loan-market/merchant/repay_result_status")
+    @Headers("Content-Type: application/json")
+    NotificationResponseDTO repayStatusNotificationDTO(RepayStatusNotificationDTO repayStatusNotificationDTO);
 
 
 
